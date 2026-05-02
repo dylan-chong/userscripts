a791b90e735907127cb72edd7664897adf009277
Dylan Chong <dylan.chong@icloud.com>
2026-05-02 17:55:00 +1200

    work

--- youtube-time-waste-blocker.user.js
+++ youtube-time-waste-blocker.user.js
@@ -13,7 +13,7 @@
     const SUBSCRIPTIONS_URL = 'https://www.youtube.com/feed/subscriptions';
 
     const CRITERIA = [
-        { whitelist: true, type: 'channelOrTitle', keywords: ['Naroditsky', 'ASMR', 'Meditation', 'Singing Bowls', 'Exercise', 'Breathing', 'Mindfulness'] },
+        { whitelist: true, type: 'channelOrTitle', keywords: ['Naroditsky', 'ASMR', 'Meditation', 'Singing Bowls', 'Exercise', 'Breathing', 'Mindfulness', 'Workout'] },
     ];
 
     function queryFirst(...selectors) {
