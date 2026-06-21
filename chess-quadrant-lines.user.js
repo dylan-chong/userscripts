f05d8933cfb87c43223a2bb0135adf0b002b06aa
Dylan Chong <dylan.chong@icloud.com>
2026-06-21 18:03:51 +1200

    MOD: chess-quadrant-lines.user.js

--- chess-quadrant-lines.user.js
+++ chess-quadrant-lines.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        chess-quadrant-lines
 // @description Draw black lines splitting the chess.com board into quadrants
-// @version     1.1
+// @version     1.2
 // @match       *://*.chess.com/*
 // @updateURL   https://raw.githubusercontent.com/dylan-chong/userscripts/main/chess-quadrant-lines.user.js
 // @downloadURL https://raw.githubusercontent.com/dylan-chong/userscripts/main/chess-quadrant-lines.user.js
@@ -10,6 +10,7 @@
 (function () {
     const LINE_ID_H = 'quadrant-line-horizontal';
     const LINE_ID_V = 'quadrant-line-vertical';
+    const THICKNESS = '6px';
 
     function createLine(id, isHorizontal) {
         const line = document.createElement('div');
@@ -25,14 +26,14 @@
                 left: '0',
                 top: '50%',
                 width: '100%',
-                height: '4px',
+                height: THICKNESS,
                 transform: 'translateY(-50%)',
             });
         } else {
             Object.assign(line.style, {
                 top: '0',
                 left: '50%',
-                width: '6px',
+                width: THICKNESS,
                 height: '100%',
                 transform: 'translateX(-50%)',
             });
