9fb9ac8dea29b3b191c022fcf8b71c307022e225
Dylan Chong <dylan.chong@icloud.com>
2026-08-19 08:32:05 +1200

    MOD: dim-mode.user.js

--- dim-mode.user.js
+++ dim-mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        dim-mode
 // @description Dim overlay for OLED screens
-// @version     2.1.1
+// @version     2.2.0
 // @match       *://*/*
 // @run-at      document-start
 // @grant       none
@@ -15,8 +15,10 @@
   const STORAGE_KEY = 'dim_mode_v1';
   const MODES = [
     { label: 'D', title: 'Dim: OFF', opacity: 0 },
-    { label: 'D', title: 'Dim: 70%', opacity: 0.7 },
-    { label: 'D', title: 'Dim: 40% brightness', opacity: 0.6 },
+    { label: 'D', title: 'Dim: 85%', opacity: 0.85 },
+    { label: 'D', title: 'Dim: 70%', opacity: 0.70 },
+    { label: 'D', title: 'Dim: 55%', opacity: 0.55 },
+    { label: 'D', title: 'Dim: 40%', opacity: 0.40 }
   ];
 
   let modeIndex = 0;
